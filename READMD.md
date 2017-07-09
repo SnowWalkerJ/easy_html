51d306959fad6715c7f4eee7e293222dba21056c
SnowWalkerJ <jike3212001@163.com>
2017-07-09 22:37:48 +0800

    update docs

---
+++ READMD.md
@@ -0,0 +1,152 @@
+# easy_html
+
+easy_html is a simple module that help quickly build html file with page.
+
+## installing
+
+This module doesn't require installing. Simply copy the file to your project path and install the required packages, and you are ready to go!
+
+## Usage
+
+### Basic Usage
+
+The basic usage of easy_html is just like writing html. Use context manager to control the opening/closing of the tags. Below is an example.
+
+```python
+from easy_html import HTML
+h = HTML()                                 # create an HTML document
+with h.use(h.body):                        # everything we do is under body
+    with h.div(_class="row"):              # a new div with class="row"
+        h.inline("h1", _text="Title")      # title
+        h.inline("hr")
+    with h.div(_class="row"):
+        h.inline("a", href="http://www.github.com", _text="Click me")
+
+print(h.render())
+```
+
+This will generate html code as below:
+```html
+<html>
+ <head>
+   <meta charset="utf-8"></meta>
+   <meta content="width=device-width, initial-scale=1" name="viewport"></meta>
+   <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+   <link type="text/css" rel="stylesheet" href="https://pingendo.github.io/templates/blank/theme.css"></link>
+ </head>
+ <body>
+   <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+   <script src="https://pingendo.com/assets/bootstrap/bootstrap-4.0.0-alpha.6.min.js"></script>
+   <div class="row">
+     <h1>Title</h1>
+     <hr></hr>
+   </div>
+   <div class="row">
+     <a href="http://www.github.com">Click me</a>
+   </div>
+ </body>
+</html>
+```
+By default, easy_html refers to static files for jQuery and Bootstrap.
+
+
+## Something Awesome
+
+As you may noticed, this style of building html is quite boring.
+So we designed something different for you.
+
+```python
+t = pd.DataFrame(
+    np.random.randn(4, 4),
+    columns=["c1", "c2", "c3", "c4"]
+)
+h = HTML(enable_highcharts=True)
+with h.use(h.body):
+    h.inline("h1", _text="Title")
+    h.inline("hr")
+container = h.container()
+row = h.row(container)
+c1, c2 = h.col(row, [6, 6])    # creates two columns in a row, split the screen half-half.
+with h.use(c1):
+    h.generate_table(t)        # generate the table on the left
+with h.use(c2):
+    h.highcharts("Chart", t)   # plot it with highcharts on the right
+print(h.render())
+```
+
+Output:
+```html
+<html>
+  <head>
+    <meta charset="utf-8"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"></link>
+    <link href="https://pingendo.github.io/templates/blank/theme.css" rel="stylesheet" type="text/css"></link>
+  </head>
+  <body>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/stock/modules/exporting.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+    <script src="https://pingendo.com/assets/bootstrap/bootstrap-4.0.0-alpha.6.min.js"></script>
+    <h1>Title</h1>
+    <hr></hr>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <table class="table table-hover table-bordered">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>c1</th>
+                <th>c2</th>
+                <th>c3</th>
+                <th>c4</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>0</td>
+                <td>-0.20</td>
+                <td>0.24</td>
+                <td>1.84</td>
+                <td>1.41</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>0.46</td>
+                <td>-0.69</td>
+                <td>0.40</td>
+                <td>1.46</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>0.47</td>
+                <td>0.84</td>
+                <td>-1.22</td>
+                <td>-0.23</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>-0.32</td>
+                <td>1.57</td>
+                <td>0.44</td>
+                <td>-0.94</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="col-md-6">
+          <div id="highcharts-Chart"></div>
+          <script>
+            Highcharts.chart('highcharts-Chart', {"series": [{"tooltip": {"valueDecimals": 3}, "name": "c1", "data": [-0.31699030142854623, -0.19920669562530138, 0.4642984624005802, 0.46570426719777414], "type": "line"}, {"tooltip": {"valueDecimals": 3}, "name": "c2", "data": [-0.6874100023606234, 0.23616310712789926, 0.8352679492902365, 1.5675330083785308], "type": "line"}, {"tooltip": {"valueDecimals": 3}, "name": "c3", "data": [-1.2208659937966237, 0.39509162417847, 0.4417390212605732, 1.8436423086892], "type": "line"}, {"tooltip": {"valueDecimals": 3}, "name": "c4", "data": [-0.9378052637051875, -0.23279546382135421, 1.4051923967078404, 1.4591743462239277], "type": "line"}], "title": {"text": "Chart"}, "xAxis": {"type": "linear"}});
+          </script>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+```
+
+Copy these in your python environment and see the result yourself!
---
+++ easy_html.py
@@ -0,0 +1,296 @@
+import json
+from html import escape
+from contextlib import contextmanager
+# from itertools import starmap
+import pandas as pd
+
+
+class Tag:
+    def __init__(self, _name, parent=None, _content=None, _inline=False, **kwargs):
+        self.name = _name
+        self.parent = parent
+        if "_class" in kwargs:
+            kwargs["class"] = kwargs["_class"]
+            del kwargs["_class"]
+        self.attrs = kwargs
+        self.inline = _inline
+        self.content = _content or []
+
+    def prepend(self, item):
+        self.content.insert(0, item)
+
+    def append(self, item):
+        self.content.append(item)
+
+    def render(self):
+        # attrs = ' '.join(starmap(lambda key, value: '%s="%s"' % (key, value), self.attrs.items()))
+        attrs = []
+        for key, value in self.attrs.items():
+            if isinstance(value, (list, tuple)):
+                value = " ".join(value)
+            attrs.append('%s="%s"' % (key, value))
+        attrs = ' '.join(attrs)
+        if attrs:
+            tag_open = '<%s %s>' % (self.name, attrs)
+        else:
+            tag_open = '<%s>' % self.name
+        tag_close = '</%s>' % self.name
+        if self.inline:
+            html = ["  {open}{content}{close}".format(
+                open=tag_open,
+                content="".join("".join(c.render()) for c in self.content),
+                close=tag_close
+            )]
+        else:
+            html = []
+            html.append(tag_open)
+            for doc in self.content:
+                html += doc.render()
+            html.append(tag_close)
+            html = ["  " + line for line in html]
+        return html
+
+
+class Text(Tag):
+    def __init__(self, text, parent=None):
+        self.text = escape(text)
+        self.parent = parent
+
+    def render(self):
+        return [self.text]
+
+
+class RawHtml(Tag):
+    def __init__(self, html, parent=None):
+        self.text = html
+        self.parent = parent
+
+    def render(self):
+        return [self.text]
+
+
+class HTMLBase:
+    """用上下文管理器生成HTML文档
+
+    Example
+    -------
+    ..  code-block:: python
+
+        doc = HTML()
+        with doc.html():
+            with doc.head():
+                doc.inline("titile", _text="Generated HTML")
+            with doc.body():
+                with doc.div(_class="container"):
+                    with doc.div(_class="row"):
+                        doc.inline("p", _text="paragraph")
+        html = doc.render()
+    """
+    def __init__(self):
+        self.root = Tag("html")
+        self.head = Tag("head")
+        self.body = Tag("body")
+        self.root.append(self.head)
+        self.root.append(self.body)
+        self.__main = [self.root]
+
+    def append(self, node):
+        self.__main.append(node)
+
+    @property
+    def main(self):
+        return self.__main[-1]
+
+    def __getattr__(self, tag_name):
+        @contextmanager
+        def wrapped(**kwargs):
+            node = Tag(tag_name, parent=self.__main, **kwargs)
+            self.main.append(node)
+            self.__main.append(node)
+            yield node
+            self.__main.pop()
+        return wrapped
+
+    def text(self, text):
+        node = Text(text)
+        self.main.append(node)
+
+    def html(self, html):
+        self.main.append(RawHtml(html))
+
+    def inline(self, tag_name, _text=None, **kwargs):
+        content = [Text(_text)] if _text else None
+        node = Tag(tag_name, **kwargs, _inline=True, _content=content)
+        self.main.append(node)
+
+    def render(self):
+        return "\n".join(self.root.render())
+
+    @contextmanager
+    def use(self, node):
+        self.__main.append(node)
+        yield node
+        self.__main.pop()
+
+
+class HTML(HTMLBase):
+    def __init__(self, enable_highcharts=False, enable_bootstrap=True):
+        super(HTML, self).__init__()
+        with self.use(self.head):
+            if enable_bootstrap:
+                self.inline("meta", charset="utf-8")
+                self.inline("meta", name="viewport", content="width=device-width, initial-scale=1")
+                self.inline("link", rel="stylesheet", href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css", type="text/css")
+                self.inline("link", rel="stylesheet", href="https://pingendo.github.io/templates/blank/theme.css", type="text/css")
+        with self.use(self.body):
+            if enable_highcharts:
+                self.inline("script", src="https://code.highcharts.com/highcharts.js")
+                self.inline("script", src="https://code.highcharts.com/stock/modules/exporting.js")
+            if enable_bootstrap:
+                self.inline("script", src="https://code.jquery.com/jquery-3.1.1.min.js")
+                self.inline("script", src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js")
+                self.inline("script", src="https://pingendo.com/assets/bootstrap/bootstrap-4.0.0-alpha.6.min.js")
+
+    def container(self):
+        node = Tag("div", _class="container", parent=self.body)
+        self.body.append(node)
+        return node
+
+    def row(self, container):
+        node = Tag("div", _class="row", parent=container)
+        container.append(node)
+        return node
+
+    def col(self, row, width=12):
+        if not isinstance(width, (list, tuple)):
+            width = (width, )
+        nodes = []
+        for w in width:
+            node = Tag("div", _class="col-md-%d" % w)
+            nodes.append(node)
+            row.append(node)
+        return nodes
+
+    def highcharts(self, name, data, plot_type=None):
+        """
+        Insert highcharts.
+
+        Parameters
+        ----------
+        name: str
+            Name of the chart.
+        data: pd.DataFrame
+            data to be converted to chart
+        plot_type: dict
+            type to be plotted
+        """
+        plot_type = plot_type or {}
+        if isinstance(plot_type, str):
+            if isinstance(data, pd.DataFrame):
+                plot_type = {col_name: plot_type for col_name in data.columns}
+            else:
+                plot_type = {data.name: plot_type}
+        self.inline('div',
+                    id="highcharts-%s" % name)
+        embedded_data = []
+        if isinstance(data, pd.DataFrame):
+            for col in data.columns:
+                series = data[col]
+                embedded_data.append((series.name, self.series2json(series)))
+        elif isinstance(data, pd.Series):
+            embedded_data.append((data.name, self.series2json(data)))
+        else:
+            raise TypeError("Data must be either DataFrame or Series")
+
+        highcharts_data = {
+            'title': {
+                'text': name
+            },
+            'series': [
+                {
+                    'name': s_name,
+                    'data': s_value,
+                    'type': plot_type.get(s_name, "line"),
+                    'tooltip': {
+                        'valueDecimals': 3
+                    }
+                }
+                for s_name, s_value in embedded_data
+            ]
+        }
+
+        if isinstance(data.index, pd.DatetimeIndex):
+            highcharts_data['xAxis'] = {'type': 'datetime'}
+        elif isinstance(data.index, pd.CategoricalIndex) or data.index.dtype.name == "object":
+            highcharts_data['xAxis'] = {'type': 'category'}
+        else:
+            highcharts_data['xAxis'] = {'type': 'linear'}
+
+        template = jinja2.Template("Highcharts.chart('highcharts-{name}', {data});".format(name=name, data=json.dumps(highcharts_data)))
+
+        with self.script():
+            self.html(template.render(name=name, data=embedded_data, plot_type=plot_type))
+
+    @staticmethod
+    def series2json(series):
+        """把pd.Series时间序列转换为json格式"""
+        series = series.copy()
+        if isinstance(series.index, pd.DatetimeIndex):
+            series.index = series.index.astype(int) / 1e6
+            return sorted([[int(key), value] for key, value in series.to_dict().items()])
+        else:
+            return sorted(series.to_dict().values())
+
+    def generate_table(self, data,
+                       show_headers=True,
+                       show_index=True,
+                       hover=True,
+                       bordered=True,
+                       striped=False,
+                       format="{:.2f}",
+                       **kwargs):
+        """Generate a table
+
+        Parameters
+        ----------
+        data: pd.DataFrame
+            data to be converted to a table
+        show_headers: bool
+            whether to show the column names as first row
+        show_index: bool
+            whether to show the index as first column
+        hover: bool
+            table-hover class
+        bordered: bool
+            table-bordered class
+        striped:
+            table-striped class
+        kwargs
+            other attributes to be added to table
+        """
+        if "_class" in kwargs:
+            kwargs["_class"].append("table")
+        else:
+            kwargs["_class"] = ["table"]
+        if hover:
+            kwargs["_class"].append("table-hover")
+        if bordered:
+            kwargs["_class"].append("table-bordered")
+        if striped:
+            kwargs["_class"].append("table-striped")
+        with self.table(**kwargs):
+            if show_headers:
+                with self.thead():
+                    with self.tr():
+                        if show_index:
+                            self.inline('th', _text=data.index.name or "#")
+                        for col in data.columns:
+                            self.inline("th", _text=col)
+            with self.tbody():
+                for idx, data in data.iterrows():
+                    with self.tr():
+                        if show_index:
+                            self.inline("td", _text=str(idx))
+                        for val in data:
+                            self.inline('td', _text=format.format(val))
+
